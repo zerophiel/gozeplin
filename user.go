793edbba4cb2050bbc13bc0e9a06fd4a3628ee98
Danuarta Wiratama <danuarta.wiratama@gmail.com>
2020-05-22 12:58:58 +0700

    Adding Get Alien User

--- user.go
+++ user.go
@@ -5,21 +5,25 @@ import (
 	"github.com/go-resty/resty/v2"
 	"time"
 )
-
+type User struct {
+	ID       string    `json:"_id"`
+	Email    string    `json:"email"`
+	Username string    `json:"username"`
+	Status   string    `json:"status"`
+	LastSeen time.Time `json:"lastSeen"`
+}
 type UserDetails struct {
 	ID      string `json:"_id"`
 	Members []struct {
 		Role       string   `json:"role"`
 		Restricted bool     `json:"restricted"`
 		Tags       []string `json:"tags"`
-		User       struct {
-			ID       string    `json:"_id"`
-			Email    string    `json:"email"`
-			Username string    `json:"username"`
-			Status   string    `json:"status"`
-			LastSeen time.Time `json:"lastSeen"`
-		} `json:"user"`
+		User       User `json:"user"`
 	} `json:"members"`
+}
+type AlienDetails struct {
+	ID      string `json:"_id"`
+	Wildlings []User `json:"wildlings"`
 }
 
 func GetOrganizationUsers (token string, organizationId string) (*UserDetails,error){
@@ -46,4 +50,18 @@ func RemoveOrganizationUser (token string, organizationId string, userId string)
 		return err
 	}
 	return nil
+}
+
+func GetOrganizationAlienUsers (token string, organizationId string) (*AlienDetails,error) {
+	var response AlienDetails
+	url := URI + "/v2/organizations/" + organizationId + "/members"
+	client := resty.New()
+	resp,err := client.R().
+		SetHeader("Content-Type", "application/json").
+		SetHeader("zeplin-token",token).Get(url)
+	if err != nil {
+		return nil, err
+	}
+	err = json.Unmarshal(resp.Body(), &response)
+	return &response, err
 }
